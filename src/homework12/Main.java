4525479e96c6748d8e5a8a50218e1aaa13d9058e
Zelimkhan Magomadov <zelimkhan.m21@gmail.com>
2021-04-04 11:33:38 +0300

    did homework_12

--- src/homework12/Main.java
+++ src/homework12/Main.java
@@ -3,90 +3,75 @@ package homework12;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
-        Main m = new Main();
-
-       /* Thread t1 = new Thread(m::method_1);
-        t1.start();
-        t1.join();*/
-
-        Thread t2 = new Thread(() -> {
-            try {
-                m.method_2();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        t2.start();
+    public static void main(String[] args) {
+        method_1();
+        method_2();
     }
 
-    private void method_1() {
-        System.out.println("Метод 1");
+    private static void method_1() {
+        System.out.println("Метод_1");
+
+        final int SIZE = 10_000_000;
+        float[] arr = new float[SIZE];
+
+        Arrays.fill(arr, 1.0f);
+        Instant start = Instant.now();
+
+        computation(arr, 0);
+
+        Instant finish = Instant.now();
+        long ms = Duration.between(start, finish).toMillis();
+        System.out.println("Время выполнения: " + ms);
+    }
+
+    private static void method_2() {
+        System.out.println("Метод_2");
 
         final int SIZE = 10_000_000;
         final int HALF = SIZE / 2;
         float[] arr = new float[SIZE];
 
+        float[] tempArray1 = new float[HALF];
+        float[] tempArray2 = new float[HALF];
+
         Arrays.fill(arr, 1.0f);
         Instant start = Instant.now();
 
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        System.arraycopy(arr, 0, tempArray1, 0, HALF);
+        System.arraycopy(arr, HALF, tempArray2, 0, HALF);
+
+        Thread t1 = new Thread(() -> computation(tempArray1, 0));
+        Thread t2 = new Thread(() -> computation(tempArray2, HALF));
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+
+            System.arraycopy(tempArray1, 0, arr, 0, HALF);
+            System.arraycopy(tempArray2, 0, arr, HALF, HALF);
+
+            Instant finish = Instant.now();
+            long ms = Duration.between(start, finish).toMillis();
+            System.out.println("Время выполнения: " + ms);
+
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
         }
-
-        Instant finish = Instant.now();
-        long ms = Duration.between(start, finish).toMillis();
-        System.out.println("arr[0] + " + arr[0]);
-        System.out.println("arr[21] + " + arr[21]);
-        System.out.println("arr[arr.length-1] + " + arr[arr.length-1]);
-        System.out.println("Время выполнения - " + ms + "мс");
     }
 
-    private void method_2() throws InterruptedException {
-        System.out.println("Метод 2");
-
-        final int SIZE = 100;
-        final int HALF = SIZE / 2;
-        float[] arr = new float[SIZE];
-
-        Arrays.fill(arr, 1.0f);
-        Instant start = Instant.now();
-
-        float[] tempArray1 = new float[HALF / 2];
-        float[] tempArray2 = new float[HALF / 2];
-
-        System.arraycopy(arr, 0, tempArray1, 0, HALF / 2);
-        System.arraycopy(arr, HALF / 2, tempArray2, 0, HALF / 2);
-
-        System.out.println("tempArray1 - " + Arrays.toString(tempArray1));
-        System.out.println("tempArray2 - " + Arrays.toString(tempArray2));
-
-        Thread t1 = new Thread(() -> calculate(tempArray1));
-        Thread t2 = new Thread(() -> calculate(tempArray2));
-
-        t1.join();
-        t2.join();
-        System.out.println();
-        System.out.println("tempArray1 - " + Arrays.toString(tempArray1));
-        System.out.println("tempArray2 - " + Arrays.toString(tempArray2));
-
-        System.arraycopy(tempArray1, 0, arr, 0, HALF / 2);
-        System.arraycopy(tempArray2, 0, arr, HALF / 2, HALF / 2);
-
-        Instant finish = Instant.now();
-        long ms = Duration.between(start, finish).toMillis();
-        System.out.println(Arrays.toString(arr));
-        System.out.println("Время выполнения - " + ms + "мс");
-    }
-
-    private void calculate(float[] arr) {
+    private static void computation(float[] arr, int coefficient) {
         for (int i = 0; i < arr.length; i++) {
-            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            arr[i] *= Math.sin(0.2f + (float) coefficient / 5) *
+                    Math.cos(0.2f + (float) coefficient / 5) *
+                    Math.cos(0.4f + (float) coefficient / 2);
+
+            coefficient++;
         }
     }
 }
